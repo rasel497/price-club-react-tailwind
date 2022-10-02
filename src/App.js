c19be15d2c2450b13de148836f028c902393614e
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-02 13:17:09 +0600

    Wow..added React Recharts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './componants/Navbar/Navbar';
 import Pricing from './componants/Pricing/Pricing';
+import AssignmentMarks from './componants/AssignmentMarks/AssignmentMarks';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1 className="text-4xl font-bold">This is a big big header</h1>
       <p>This is just a paragraph.</p>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
---
+++ src/componants/PhoneBar/PhoneBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhoneBar = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PhoneBar;
